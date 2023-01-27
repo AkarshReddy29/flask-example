6f6c2ae361dcb321fff0d01cffad2e28ba1e9c65
AkarshReddy29 <reddyakarsh@yahoo.in>
2023-01-27 19:13:07 +0530

    Revert "third"
    This reverts commit 2998a9df96a1ce789c953a5f1751f8636fbc80ac.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!! Flask Flask flask"
+    return "Flask inside Docker!! Flask Flask"
 
 
 if __name__ == "__main__":
